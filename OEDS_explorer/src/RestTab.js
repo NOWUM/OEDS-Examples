0582b2826a65f26ced35efa482a82b9d0aa1b21c
vassily aliseyko <valiseyko@googlemail.com>
2024-07-11 15:34:46 +0200

    swagger try it out uses correct postgrest headers

--- OEDS_explorer/src/RestTab.js
+++ OEDS_explorer/src/RestTab.js
@@ -1,18 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import { DBContext } from './DBContext';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 import './RestTab.css';
+
 function RestTab() {
-    const { swaggerSpec, swaggerOptions } = useContext(DBContext);
-    const isLoading = !swaggerSpec || !swaggerOptions.length;
-    
+    const { swaggerSpec, swaggerOptions, selectedProfile } = useContext(DBContext);
+    const isLoading = !swaggerSpec || !swaggerOptions;
+
     return (
         <>
             <div className="rest-tab">
                 {isLoading && <div className="loading-container">Loading...</div>}
-                {!isLoading && swaggerSpec && <SwaggerUI spec={swaggerSpec} />}
+                {!isLoading && swaggerSpec && (
+                    <Swagger
+                        swaggerSpec={swaggerSpec}
+                        selectedProfile={selectedProfile}
+                    />
+                )}
             </div>
+        </>
+    );
+}
+
+function Swagger({ swaggerSpec, selectedProfile }) {
+    const profileRef = useRef(selectedProfile);
+
+    useEffect(() => {
+        profileRef.current = selectedProfile; 
+    }, [selectedProfile]);
+
+    const getSwaggerConfig = () => ({
+        ...swaggerSpec,
+        requestInterceptor: (request) => {
+            request.headers['Accept-Profile'] = profileRef.current;
+            request.headers['Content-Profile'] = profileRef.current;
+            return request;
+        }
+    });
+
+    const config = getSwaggerConfig();
+
+    return (
+        <>
+            <SwaggerUI spec={swaggerSpec} {...config} />
         </>
     );
 }
